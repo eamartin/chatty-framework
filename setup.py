534399f41ee70e601cc0119958aca31071e85d13
lightcatcher <e.a.martin1337@gmail.com>
2010-12-05 17:50:52 -0600

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'chatty',
+    version = '.1',
+    packages = ['chatty',],
+    long_description = open('README.txt').read(),
+    author = 'Eric Martin',
+    author_email  = 'e.a.martin1337@gmail.com',
+)
